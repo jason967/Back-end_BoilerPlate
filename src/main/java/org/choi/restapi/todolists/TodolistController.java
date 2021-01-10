fcf09ae314af792c91e71da484c437e0b01824f0
최재웅 <jason967@naver.com>
2021-01-11 04:26:06 +0900

    [ADD] createdTodolistController

--- src/main/java/org/choi/restapi/todolists/TodolistController.java
+++ src/main/java/org/choi/restapi/todolists/TodolistController.java
@@ -1,15 +1,28 @@
 package org.choi.restapi.todolists;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+//HAL_JSON형식으로 응답을 보낸다.
+@RequestMapping(value ="/api/todolist" ,produces = MediaTypes.HAL_JSON_VALUE)
 public class TodolistController {
 
-    @GetMapping("/hello")
-    public String HelloController()
+    @PostMapping
+    public ResponseEntity createTodolist(@RequestBody Todolist todolist)
     {
-        return "hello";
+        //TODO methodOn의 용도 정확하게 알아보기
+        //created를 보낼 URI가 필요함-> 만들어야됌 hateos가 제공하는 linkTo(),methodOn()사용
+        URI createdUri = linkTo(TodolistController.class).slash("{id}").toUri();
+        todolist.setId(10L);
+       return ResponseEntity.created(createdUri).body(todolist);
     }
 }
--- src/test/java/org/choi/restapi/todolists/TodolistControllerTest.java
+++ src/test/java/org/choi/restapi/todolists/TodolistControllerTest.java
@@ -1,5 +1,6 @@
 package org.choi.restapi.todolists;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -20,15 +23,34 @@ public class TodolistControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    //TODO  ObjectMapper objectMapper의 역할 정확하게 파악하하기
+   @Autowired
+    ObjectMapper objectMapper;
+
+    //1. 테스팅을 하면 당연히 실패한다.
     @Test
     public void createTodolist() throws Exception {
+
+        Todolist todolist = Todolist.builder()
+                .name("choi")
+                .content("content is here")
+                .startDate(LocalDateTime.of(2021,01,11,12,00,00))
+                .endDate(LocalDateTime.of(2021,01,12,12,00))
+                .build();
+
         mockMvc.perform(post("/api/todolist/")
                 //요청본문에 JSON을 담아서 보낸다.
                 .contentType(MediaType.APPLICATION_JSON)
                 //HAL_JSON형식의 응답을 원한다.
-                .accept(MediaTypes.HAL_JSON))
+                .accept(MediaTypes.HAL_JSON)
+                //todolist객체를 json형식의 string으로 변환해줌-> 요청본문에 넣어줌
+                .content(objectMapper.writeValueAsString(todolist)))
                 //Response 201이 나오길 기대한다.
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                //TODO jsonPath의 용도 파악하기
+                //"id"의 값이 존재하는 지 테스팅
+                .andExpect(jsonPath("id").exists())
+                .andDo(print());
     }
 
 }
